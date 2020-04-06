12b76e64216eb63e5fa5605ccb4a2d8d3a38db3a
Mason Balbera <mhbalbera@gmail.com>
2020-04-06 11:43:44 -0400

    set up namespacing

--- todolist-backend-api/config/routes.rb
+++ todolist-backend-api/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tasks, only: [:index, :create, :destroy]
+  namespace :api do 
+    namespace :v1 do 
+      get 'tasks/', :to => 'tasks#index'
+      post 'tasks/new', :to => 'tasks#create'
+      delete 'tasks/delete/:id', :to => 'tasks#destroy'
+    end
+  end
 end
