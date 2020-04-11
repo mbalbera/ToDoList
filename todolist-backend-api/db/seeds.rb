2ff43078dd8e7818560e333001756e03e003885b
Mason Balbera <mhbalbera@gmail.com>
2020-04-11 17:03:10 -0400

    update seeds

--- todolist-backend-api/db/seeds.rb
+++ todolist-backend-api/db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: 'Mason')
-Task.create(user_id: 1, completed: false, time: "8:00 am", text: "breakfast time")
-Task.create(user_id: 1, completed: true, time: "9:30 am", text: "gym time")
-Task.create(user_id: 1, completed: false, time: "11:00 pm", text: "bed time")
+User.create(name: 'Elijah')
+Task.create(user_id: 1, completed: false, time: "8:00 am", text: "schedule interview with Mason ")
+Task.create(user_id: 1, completed: true, time: "9:30 am", text: "zoom with Mason")
+Task.create(user_id: 1, completed: false, time: "11:00 pm", text: "hire Mason")
